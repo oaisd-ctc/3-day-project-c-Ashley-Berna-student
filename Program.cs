c07312f61dff1beba71dcc77cd7b97a6e56ecc95
Keegan-Ohlman-Student <144161693+Keegan-Ohlman-Student@users.noreply.github.com>
2024-01-22 18:19:04 +0000

    Added more to instructions

--- Program.cs
+++ Program.cs
@@ -7,9 +7,8 @@
         System.Console.WriteLine("You have 6 lives. You will input a single letter at a time.");
         System.Console.WriteLine("The game will then tell you if the word has any of that letter in it.");
         System.Console.WriteLine("If the word does have some of that letter the game will reprint out the word \nwith the unguessed letters as blanks and the correct letters filled in the word. ");
+        System.Console.WriteLine("If there are multiple words there will be spaces in between the astericks.");
         System.Console.WriteLine("And that covers it. Very complicated I know. All I have left to say is good luck.");
-
-        //hi
 
         Random random = new Random();
 
